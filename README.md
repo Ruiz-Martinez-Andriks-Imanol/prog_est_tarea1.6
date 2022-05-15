6fa276137ebd9d73b0605929ab17c739daf07473
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-15 14:34:27 -0500

    prog_est_tarea1.6
    Programa para calcular el área y el volumen de un trapecio

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # prog_est_tarea1.6
-Calcular el área y volumen de un tetraedro
+<p>Autor: Andriks Imanol Ruiz Mártinez, Fecha: 03/02/2022 <br>
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras, flotantes y doubles</li>
+    <li>printf para mostrar mensajes y variables</li>
+    <li>scanf</li>
+    <li>El uso de include para las librerías</li>
+    <li>funciones de la libreria math.h</li>
+    <li>Comentarios para la documentación interna del programa</li>
+    </ul>
+    </p>
+<br>
+Objetivo: Hacer un programa que calcule el área y el volumen de un tetraedro.
+<br>
+<br>
+<p>Breve descripción:
+Es un programa para calcular el área y el volumen de un tetraedro, usando la librería math.h y las variables double.
+<br>
+</p>
---
+++ tarea1.6.cpp
@@ -0,0 +1,38 @@
+/* Autor:	Andriks Imanol Ruiz Martínez, Realizado:03/02/22
+Escuela: Universidad del Valle de Mexico Campus Villahermosa
+Materia: Programación Estructrada
+Ciclo: 01/2022
+
+Este es un programa en Lenguaje C de la materia de Programación Estructurada
+Muestra el uso de:
+  
+    -Variables enteras, flotantes y doubles
+    -printf para mostrar mensajes y variables
+    -scanf
+    -El uso de include para las librerías
+    -funciones de la libreria math.h
+    -Comentarios para la documentación interna del programa
+	*/
+#include <math.h>
+#include <stdio.h>
+int main(int argc, char const *argv[]){
+	//Declaración de variables
+	int a;
+	float volumen, area;
+	double numero1 = 3;
+	double raizCuadrada = sqrt(numero1);
+	double numero2 = 2;
+	double raizCuadrada2 =sqrt(numero2);
+	//Entrada de datos
+	printf("CALCULADORA DE AREA Y VOLUMEN DE TETAEDRO REGULAR\n\n");
+	printf("Ingresa a: ");
+	scanf("%d",&a);
+	//Proceso
+	area=raizCuadrada*(a*a);
+	//volumen
+	volumen=raizCuadrada2*(a*a)/12;
+	//Salida
+	printf("El area del tetaedro regular es: %f\n",area);
+	printf("El volumen del tetaedro regular es: %f",volumen);
+return 0;
+}
